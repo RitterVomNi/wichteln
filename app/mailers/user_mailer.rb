9a06e33288bf2d8556af6d05a936f518e8bba7f7
RitterVomNi <heinen.m@web.de>
2016-05-14 19:11:28 +0200

    muh

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def schicken(user, name, email)
     @user = user
     @name = name
-    mail(to: email, subject: 'Weihnachtswichteln')
+    mail(from: "Weihnachtsmann", to: email, subject: 'Weihnachtswichteln')
   end
 end
